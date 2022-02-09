7420f2f43a413b25e9bd75b6d1ece4a92b56aad7
Amro Elmo <aelmohtaseb@better.com>
2022-02-09 10:36:58 -0800

    Add Skeleton for website

--- scripts/server.js
+++ scripts/server.js
@@ -3,14 +3,7 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
-
-//add the router
-app.use('/', router);
+app.use('/', express.static(path.join(__dirname, '..')));
 app.listen(process.env.port || 3300);
 
 console.log('Running at Port 3300');
