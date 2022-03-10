a17a1b43cf32492fe164c4a91f23c1a949f6bf8d
Amro Elmo <aelmohtaseb@better.com>
2022-03-09 17:54:11 -0800

    removing node stuff

---
+++ scripts/eiclock.js
@@ -0,0 +1,51 @@
+/**
+ * eiclock.js
+ * Author: A7a Ezay
+ * All Rights Reserved Bla Bla Bla
+ * 
+ * This is a clock based on 
+ * Euler's Identity Equation e^i*pi = 1 Day.
+ * The clock rotation is anti-clock wise (ironically)
+ * 
+ * The clock divide the day into 4 quarters, each 6 hours.
+ * 
+ * 
+ */
+
+ let r;
+ let len;
+ let eulerFont;
+
+function preload() {
+  eulerFont = loadFont('../fonts/neo-euler.otf');
+}
+
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("eiclock-canvas");
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  textFont(eulerFont);
+  drawTangentLine(windowWidth / 2, windowHeight / 2);
+}
+
+
+function drawLogo(cx, cy) {
+  for (let theta = 0; theta < TWO_PI; theta += 0.3) {
+    let x = r * cos(theta);
+    let y = r * sin(theta);
+    push();
+    translate(cx, cy);
+    rotate(theta);
+    textSize(128);
+    text(' i π', 100, 100);
+    pop();
+  }
+  textSize(360);
+  text('e', cx-96, cy+80)
+}
--- scripts/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
-
-app.use('/', express.static(path.join(__dirname, '..')));
-app.listen(process.env.port || 3300);
-
-console.log('Running at Port 3300');
